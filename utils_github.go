3cf3ea6894b4c7b2cfd7faac84f9dcbb13a56dd0
paulie-of-punskas <pbernecki@ik.me>
2026-09-12 11:43:20 +0200

    GitHub integration done

--- utils_github.go
+++ utils_github.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 // Function looks for GitHub PAT in following order:
-// 1. .env, 2. env variable GITHUB_PAT
+// 1. .env, 2. environmental GITHUB_PAT variable
 func GetPAT() string {
 
 	if patContent := os.Getenv("GITHUB_PAT"); patContent != "" {
@@ -23,5 +25,26 @@ func GetPAT() string {
 }
 
 // Using getPAT() results, check if GitHub API returns 200
-func checkGitHubConnection() {
+// github PAT is optional - depends on a reposiory settings
+func isConnectionToGitHubPossible(repoOwner string, repoName string, githubPAT string) bool {
+	client := &http.Client{}
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/actions/runs", repoOwner, repoName)
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("Accept", "application/vnd.github+json")
+	if githubPAT != "" {
+		req.Header.Set("Authorization", "Bearer "+githubPAT)
+	}
+	req.Header.Set("X-GitHub-Api-Version", "2026-03-10")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalf("Could not send GET request to %s/%s: %s", repoOwner, repoName, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		return true
+	}
+	return false
 }
--- utils_github_test.go
+++ utils_github_test.go
@@ -5,11 +5,20 @@ import (
 )
 
 func TestGetPAT(t *testing.T) {
-	t.Run("Should return empty string, if .env and GITHUB_PAT are not available.", func(t *testing.T) {
+	t.Run("Should return a non empty string, if .env is available.", func(t *testing.T) {
 		var got = GetPAT()
-		var want = ""
-		if got != want {
+		if got == "" {
 			t.Errorf("Expected empty string, got %v", got)
+		}
+	})
+}
+
+func TestIsConnectionToGitHubPossible(t *testing.T) {
+	t.Run("Should return true", func(t *testing.T) {
+		var got = isConnectionToGitHubPossible("paulie-of-punskas", "gha-costs", "")
+		var want = true
+		if got != want {
+			t.Errorf("Expected true to be returned")
 		}
 	})
 }
