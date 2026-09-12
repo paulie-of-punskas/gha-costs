b4c1355f623aa8f039402a3e85d184f0938ba2e8
paulie-of-punskas <pbernecki@ik.me>
2026-09-12 16:03:00 +0200

    Added initial flags for the CLI

--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
-	"flag"
+	// "flag"
 	"fmt"
-	"log"
+	"os"
 )
 
 var appVersion = "1.0.0"
 var gitHubPat = ""
 
 func main() {
-	printAbout := flag.Bool("about", false, "Print information about this app.")
+	// set flags
+	// aboutCmd := flag.NewFlagSet("about", flag.ExitOnError)
+	// connectionCmd := flag.NewFlagSet("check_connection", flag.ExitOnError)
+	// connectionRepoOwnerAndRepoName := connectionCmd.String("repoOwnerName", "", "Owner of the repository and repo name")
 
-	flag.Parse()
+	if len(os.Args) == 1 {
+		fmt.Printf("Available flags:\n%s%s", "-about\n", "-check_connection")
+		os.Exit(0)
+	}
 
-	if *printAbout {
+	switch os.Args[1] {
+	case "check_connection":
+		// connectionCmd.Parse(os.Args[2:])
+		userInput := os.Args[2]
+		if len(userInput) > 0 {
+			fmt.Printf("Checking if connection is possible for %s... ", userInput)
+			if IsConnectionToGitHubPossible(userInput, false) {
+				fmt.Println("Success!")
+			}
+		} else {
+			fmt.Println("Please provide repository owner and repo name, e.g. 'paulie-of-punskas/gha-costs'")
+		}
+	case "about":
 		author := "paulie-of-punskas, a.k.a. paulie-aus-punskas"
 		appAbout := "This is a CLI tool, that is used for monitoring costs of GitHub Actions runners" +
 			"\nNo data is transmitted outside of URLs defined by user." +
 			"\nNo AI data center was used to produce this code."
-		log.Printf("[Author]\n%s \n\n[App version]\n%s \n\n[About]\n%s", author, appVersion, appAbout)
-	} else {
-		fmt.Printf("Available flags:\n%s%s", "-about\n", "-check_connection\n")
+		fmt.Printf("[Author]\n%s \n\n[App version]\n%s \n\n[About]\n%s\n", author, appVersion, appAbout)
 	}
 }
--- utils_github.go
+++ utils_github.go
@@ -10,14 +10,13 @@ import (
 // Function looks for GitHub PAT in following order:
 // 1. .env, 2. environmental GITHUB_PAT variable
 func GetPAT() string {
-
-	if patContent := os.Getenv("GITHUB_PAT"); patContent != "" {
-		return patContent
-	}
-
 	envFileContent, err := os.ReadFile(".env")
 	if err == nil {
 		return string(envFileContent)
+	}
+
+	if patContent := os.Getenv("GITHUB_PAT"); patContent != "" {
+		return patContent
 	}
 
 	fmt.Println("Neither .env, nor GITHUB_PAT were found.")
@@ -25,21 +24,21 @@ func GetPAT() string {
 }
 
 // Using getPAT() results, check if GitHub API returns 200
-// github PAT is optional - depends on a reposiory settings
-func isConnectionToGitHubPossible(repoOwner string, repoName string, githubPAT string) bool {
+// github PAT is optional - depends on how user had setup repository
+func IsConnectionToGitHubPossible(repoOwnerAndName string, useGithubPAT bool) bool {
 	client := &http.Client{}
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/actions/runs", repoOwner, repoName)
+	url := fmt.Sprintf("https://api.github.com/repos/%s/actions/runs", repoOwnerAndName)
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Set("Accept", "application/vnd.github+json")
-	if githubPAT != "" {
-		req.Header.Set("Authorization", "Bearer "+githubPAT)
+	if useGithubPAT {
+		req.Header.Set("Authorization", "Bearer "+GetPAT())
 	}
 	req.Header.Set("X-GitHub-Api-Version", "2026-03-10")
 
 	resp, err := client.Do(req)
 
 	if err != nil {
-		log.Fatalf("Could not send GET request to %s/%s: %s", repoOwner, repoName, err)
+		log.Fatalf("Could not send GET request to %s: %s", repoOwnerAndName, err)
 	}
 	defer resp.Body.Close()
 
