0856e0c23f0ec231ef442e427414ac460d0894da
paulie-of-punskas <10699712+paulie-of-punskas@users.noreply.github.com>
2026-09-13 09:44:54 +0200

    Update test for IsConnectionToGitHubPossible

--- utils_github_test.go
+++ utils_github_test.go
@@ -13,12 +13,12 @@ func TestGetPAT(t *testing.T) {
 	})
 }
 
-// func TestIsConnectionToGitHubPossible(t *testing.T) {
-// 	t.Run("Should return true", func(t *testing.T) {
-// 		var got = IsConnectionToGitHubPossible("paulie-of-punskas/gha-costs", false)
-// 		var want = true
-// 		if got != want {
-// 			t.Errorf("Expected true to be returned")
-// 		}
-// 	})
-// }
+func TestIsConnectionToGitHubPossible(t *testing.T) {
+	t.Run("Should return true", func(t *testing.T) {
+		var got = IsConnectionToGitHubPossible("paulie-of-punskas/gha-costs", false)
+		var want = true
+		if got != want {
+			t.Errorf("Expected true to be returned")
+		}
+	})
+}
